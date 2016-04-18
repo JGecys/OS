8b1f99a08f150f5a45d4001c07dd9def5e633582
JurgisG <jgecys@gmail.com>
2016-04-18 12:53:08 +0300

    main include update

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
-#include<sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <aio.h>
 
 #define BUFFLEN 1048576
 
